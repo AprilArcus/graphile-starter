33e7cd5e466d8447abd7cd8fc394f7d165a5454d
April Arcus <april.arcus@gmail.com>
2026-04-03 22:07:07 -0700

    Node -> 18
    - Node 18 changes the resolution of `localhost` to ::1 (ipv6) instead of
      127.0.0.1 (ipv4). In CI, we must tell Docker to map port 5432 (postgres)
      on ipv6 instead of ipv4.
    
    - Node 18 no longer spawns yarn.cmd with { shell: true } by default, so
      in `scripts/lib/run.js`, we now set it explicitly if we detect that
      the OS is win32.

--- scripts/_setup_utils.js
+++ scripts/_setup_utils.js
@@ -1,5 +1,5 @@
-if (parseInt(process.version.split(".")[0], 10) < 10) {
-  throw new Error("This project requires Node.js >= 10.0.0");
+if (parseInt(process.version.split(".")[0], 18) < 18) {
+  throw new Error("This project requires Node.js >= 18.0.0");
 }
 
 const fsp = require("fs").promises;
